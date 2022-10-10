663b89fb72f3e0b49a9e0332423af9c237f904e8
JuHyun419 <zzang9ha@naver.com>
2022-10-10 21:58:00 +0900

    initial commit

---
+++ src/main/java/com/example/java/deepdive/JavaDeepDiveApplication.java
@@ -0,0 +1,13 @@
+package com.example.java.deepdive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDeepDiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaDeepDiveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/java/deepdive/JavaDeepDiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java.deepdive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDeepDiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
